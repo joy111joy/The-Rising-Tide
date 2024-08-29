2b68b74b80958f13375cb658d85a1934b8b68d0a
Mitchel <mitchel_joy@hotmail.com>
2024-08-29 03:09:01 -0230

    test commit

--- rising-tide/src/App.js
+++ rising-tide/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
-
+//test
 function App() {
   return (
     <div className="App">
